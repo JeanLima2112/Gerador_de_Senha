adb6da937ac0604dbda96f8b472778c3ed796d7c
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-07-26 15:53:22 -0300

    project

---
+++ script.js
@@ -0,0 +1,30 @@
+let button = document.querySelector('#button');
+let slider =document.querySelector('#slider');
+
+let password = document.querySelector('#password');
+let size = document.querySelector('#valor');
+
+let container = document.querySelector('#container-password');
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%,.:¨&*_+=-[];/?';
+let Novasenha = "";
+
+size.innerHTML = slider.value;
+
+slider.oninput = function(){
+    size.innerHTML = this.value;
+}
+
+function generatepassword(){
+    let pass = "";
+    for(let i=0, n= charset.length; i<slider.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()* n));
+    }
+    container.classList.remove('hide');
+    password.innerHTML = pass
+    Novasenha =pass;
+
+}
+ function copypassword(){
+    alert('Senha copiada!')
+    navigator.clipboard.writeText(Novasenha)
+ }
